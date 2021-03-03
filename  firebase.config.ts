db90ea5e14f4d9bd2a5dcfdeca9fda2efc2394ce
andreia23 <andreialima.berto08@gmail.com>
2021-03-03 18:40:57 -0300

    feat: firebase configuration

---
+++  firebase.config.ts
@@ -0,0 +1,12 @@
+export const FirebaseConfig = {
+  firebase: {
+    apiKey: 'AIzaSyBr5uKPElp08C1YKcNKphvG45qRSFbNm04',
+    authDomain: 'projeto-gps-p5.firebaseapp.com',
+    projectId: 'projeto-gps-p5',
+    storageBucket: 'projeto-gps-p5.appspot.com',
+    messagingSenderId: '46049561191',
+    appId: '1:46049561191:web:188d90b6b607ec452b22c2',
+    measurementId: 'G-0VCZ162NGT'
+
+  }
+};
